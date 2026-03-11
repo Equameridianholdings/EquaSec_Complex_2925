87a88108ab4e35942a5dade783e37e54c8c58d06
Its-JustSbu <116superstm@gmail.com>
2026-03-11 19:02:33 +0200

    added await for asyncronise db connection

--- backend/server.ts
+++ backend/server.ts
@@ -6,23 +6,22 @@ const URI = process.env.MONGODB_URI as unknown as string;
 const DB_NAME = process.env.DB_NAME as unknown as string;
 
 // MongoDB Connection
-void mongoose.connect(URI, {
-  dbName: DB_NAME,
-  maxPoolSize: 150,
-  serverApi: {
-    deprecationErrors: true,
-    strict: true,
-    version: ServerApiVersion.v1,
-  },
-  timeoutMS: 10000,
-});
+const connectToDB = async () => {
+  await mongoose.connect(URI, {
+    dbName: DB_NAME,
+    maxPoolSize: 150,
+    serverApi: {
+      deprecationErrors: true,
+      strict: true,
+      version: ServerApiVersion.v1,
+    },
+    timeoutMS: 10000,
+  });
+  await mongoose.connection.db?.admin().command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+};
 
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log(`Connected to MongoDB`);
-});
+connectToDB().catch(console.dir);
 
 const PORT = Number.parseInt(process.env.PORT as unknown as string) || 3000;
 
